baa5f528979679014eded216df0296adc711122c
peculiarnewbie <32804415+peculiarnewbie@users.noreply.github.com>
2023-10-17 14:20:31 +0700

    fix import error

--- src/routes/tiff/show/+page.server.ts
+++ src/routes/tiff/show/+page.server.ts
@@ -1,81 +1,81 @@
-import { gzip, gunzip } from 'zlib';
-// import * as gz from '../../lib/components/images/test.json.gz';
-import dataJson from '../../lib/components/images/data.json';
-import fs from 'fs';
+// import { gzip, gunzip } from 'zlib';
+// // import * as gz from '../../lib/components/images/test.json.gz';
+// // import dataJson from '../../lib/components/images/data.json';
+// import fs from 'fs';
 
-export async function load({ fetch }) {
-	let heightData: number[][] = new Array(2000);
+// export async function load({ fetch }) {
+// 	let heightData: number[][] = new Array(2000);
 
-	for (let i = 0; i < heightData.length; i++) {
-		heightData[i] = new Array(2000); // Initialize the inner arrays with size 100
-	}
+// 	for (let i = 0; i < heightData.length; i++) {
+// 		heightData[i] = new Array(2000); // Initialize the inner arrays with size 100
+// 	}
 
-	for (let i = 0; i < 2000; i++) {
-		for (let j = 0; j < 2000; j++) {
-			heightData[i][j] = 0;
-		}
-	}
+// 	for (let i = 0; i < 2000; i++) {
+// 		for (let j = 0; j < 2000; j++) {
+// 			heightData[i][j] = 0;
+// 		}
+// 	}
 
-	let canIT = false;
+// 	let canIT = false;
 
-	Object.entries(dataJson).forEach((entry) => {
-		const [key, value] = entry;
-		Object.entries(value).forEach((entry2) => {
-			const [key2, value2] = entry2;
-			let i = key as number;
-			let j = key2 as number;
-			heightData[i][j] = value2;
-		});
-		// console.log(key, value);
-	});
+// 	Object.entries(dataJson).forEach((entry) => {
+// 		const [key, value] = entry;
+// 		Object.entries(value).forEach((entry2) => {
+// 			const [key2, value2] = entry2;
+// 			let i = key as number;
+// 			let j = key2 as number;
+// 			heightData[i][j] = value2;
+// 		});
+// 		// console.log(key, value);
+// 	});
 
-	// const json = await loadFromZip();
+// 	// const json = await loadFromZip();
 
-	return {
-		heightData: heightData
-	};
+// 	return {
+// 		heightData: heightData
+// 	};
 
-	// console.log('buff', res);
-	// const json = await JSON.parse(res);
-	// console.log('json: ', json);
-	// heightData = json;
-}
+// 	// console.log('buff', res);
+// 	// const json = await JSON.parse(res);
+// 	// console.log('json: ', json);
+// 	// heightData = json;
+// }
 
-async function loadFromZip() {
-	let res: string = '';
+// async function loadFromZip() {
+// 	let res: string = '';
 
-	let json;
+// 	let json;
 
-	let event = false;
+// 	let event = false;
 
-	async function assignFile(error, buff) {
-		if (error != null) {
-		} else {
-			res = buff.toString();
-			json = await JSON.parse(res);
-			event = true;
-		}
-	}
+// 	async function assignFile(error, buff) {
+// 		if (error != null) {
+// 		} else {
+// 			res = buff.toString();
+// 			json = await JSON.parse(res);
+// 			event = true;
+// 		}
+// 	}
 
-	const data = fs.readFileSync('../../lib/components/images/test.json.gz', {
-		encoding: 'binary'
-	});
+// 	const data = fs.readFileSync('../../lib/components/images/test.json.gz', {
+// 		encoding: 'binary'
+// 	});
 
-	const buffer = Buffer.from(data, 'binary');
+// 	const buffer = Buffer.from(data, 'binary');
 
-	gunzip(buffer, assignFile);
+// 	gunzip(buffer, assignFile);
 
-	async function waitForEvent() {
-		return new Promise(function (resolve, reject) {
-			setInterval(function () {
-				if (event) resolve('lezz goo');
-			}, 100);
-		});
-	}
+// 	async function waitForEvent() {
+// 		return new Promise(function (resolve, reject) {
+// 			setInterval(function () {
+// 				if (event) resolve('lezz goo');
+// 			}, 100);
+// 		});
+// 	}
 
-	await waitForEvent();
+// 	await waitForEvent();
 
-	console.log('whaat');
+// 	console.log('whaat');
 
-	return json;
-}
+// 	return json;
+// }
